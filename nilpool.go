50b25d73c6abbccb7c041219f74215a250d36325
Jason Mooberry <jason.mooberry@gmail.com>
2014-11-19 13:23:22 -0500

    moar nil

--- nilpool.go
+++ nilpool.go
@@ -4,10 +4,10 @@ import "time"
 
 type NilPool struct{}
 
-func NewNilPool() *NilPool {
-	return &NilPool{}
+func NewNilPool() NilPool {
+	return NilPool{}
 }
 
-func (_ *NilPool) Count(_ string, _ float64)              {}
-func (_ *NilPool) Value(_ string, _ float64, _ time.Time) {}
-func (_ *NilPool) Duration(_ string, _ time.Duration)     {}
+func (_ NilPool) Count(_ string, _ float64)              {}
+func (_ NilPool) Value(_ string, _ float64, _ time.Time) {}
+func (_ NilPool) Duration(_ string, _ time.Duration)     {}
