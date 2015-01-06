e7fc49bb1546b4a6b8ffa04f27c461e7f9468f3f
Jason Mooberry <jason.mooberry@gmail.com>
2015-01-06 17:11:01 -0500

    Adding logger pool

---
+++ loggerpool.go
@@ -0,0 +1,33 @@
+package statpool
+
+import (
+	"log"
+	"math/rand"
+	"time"
+)
+
+type LoggerPool struct {
+	l *log.Logger
+}
+
+func NewLoggerPool(logger *log.Logger) LoggerPool {
+	return LoggerPool{l: logger}
+}
+
+func (l *LoggerPool) Count(key string, val float64) {
+	l.l.Printf("%s:%g", key, val)
+}
+
+func (l *LoggerPool) Value(key string, val float64, _ time.Time) {
+	l.l.Printf("%s:%g", key, val)
+}
+
+func (l *LoggerPool) Duration(key string, val time.Duration) {
+	l.l.Printf("%s:%s", key, val)
+}
+
+func (l *LoggerPool) SampledDuration(key string, val time.Duration, rate float64) {
+	if rate < rand.Float64() {
+		l.l.Printf("%s:%s", key, val)
+	}
+}
--- statpool.go
+++ statpool.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"os"
 	"sync"
-
 	"time"
 )
 
